84a414e5589582770fba37313d4b4f49663f344d
Theodore Rivaldo Devo Mangowal <87703322+theodevoid@users.noreply.github.com>
2023-11-09 00:23:34 +0700

    ui: revamp player list

--- apps/web/src/features/events/event-details/components/TeamListItem.tsx
+++ apps/web/src/features/events/event-details/components/TeamListItem.tsx
@@ -4,9 +4,11 @@ import { useGetPlayersFromEventQuery } from '@v6/api';
 import { MlbbRole, Prisma } from '@v6/db';
 
 import { mlbbRoleEnumToText } from '~/utils/role';
+import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar';
 import { Badge } from '~/components/ui/badge';
 import { Button } from '~/components/ui/button';
-import { Label } from '~/components/ui/label';
+import { Input } from '~/components/ui/input';
+import { ScrollArea } from '~/components/ui/scroll-area';
 import {
   Select,
   SelectContent,
@@ -17,6 +19,7 @@ import {
 import {
   Sheet,
   SheetContent,
+  SheetFooter,
   SheetHeader,
   SheetTitle,
 } from '~/components/ui/sheet';
@@ -77,13 +80,13 @@ export const TeamListItem: React.FC<TeamListItemProps> = ({
       })}
 
       <Sheet open={sheetOpened} onOpenChange={setSheetOpened}>
-        <SheetContent side="bottom">
-          <SheetHeader className="text-left">
+        <SheetContent
+          side="bottom"
+          className="flex h-[95vh] flex-col justify-between"
+        >
+          <SheetHeader className="mb-2 text-left">
             <SheetTitle>Tambah player</SheetTitle>
-          </SheetHeader>
-          <div className="my-4">
-            <div className="space-y-1">
-              <Label htmlFor="mlbb-role">Pilih role</Label>
+            <div className="grid grid-cols-2 gap-2">
               <Select
                 value={searchRole}
                 onValueChange={(value) => setSearchRole(value as MlbbRole)}
@@ -99,34 +102,81 @@ export const TeamListItem: React.FC<TeamListItemProps> = ({
                   ))}
                 </SelectContent>
               </Select>
+              <Input placeholder="Search nickname" />
             </div>
-            <Button variant="outline" className="my-2 w-full">
+            <Button variant="secondary" className="mt-2 w-full">
               Random player <ShuffleIcon className="ml-2" />
             </Button>
+          </SheetHeader>
+          <ScrollArea className="h-full pb-2">
+            <div className="flex flex-col gap-2">
+              {registeredPlayers
+                ?.filter(
+                  (registeredPlayer) => registeredPlayer.role === searchRole,
+                )
+                .map(({ player, role, profileUserId }) => {
+                  const {
+                    displayName,
+                    profilePictureUrl,
+                    mlbbUsername,
+                    mlbbUserId,
+                    mlbbServerId,
+                  } = player;
 
-            {registeredPlayers
-              ?.filter(
-                (registeredPlayer) => registeredPlayer.role === searchRole,
-              )
-              .map((registeredPlayer) => {
-                return (
-                  <div
-                    key={registeredPlayer.profileUserId}
-                    className="flex items-center justify-between rounded-md border p-2"
-                  >
-                    <span className="text-sm">
-                      {registeredPlayer.player.mlbbUsername}
-
-                      <span className="ml-1 text-muted-foreground">
-                        ({mlbbRoleEnumToText(registeredPlayer.role)})
-                      </span>
-                    </span>
-
-                    <Button size="sm">Tambah ke tim</Button>
-                  </div>
-                );
-              })}
-          </div>
+                  return (
+                    <div
+                      key={profileUserId}
+                      className="group flex items-center justify-between gap-4 rounded-md border p-4"
+                    >
+                      <div className="flex w-16 flex-col flex-wrap justify-center gap-1.5">
+                        <Avatar className="h-16 w-16 rounded-md">
+                          <AvatarImage src={profilePictureUrl || ''} />
+                          <AvatarFallback>
+                            {displayName?.charAt(0)}
+                          </AvatarFallback>
+                        </Avatar>
+                        <p className="text-center text-sm text-muted-foreground">
+                          {displayName}
+                        </p>
+                      </div>
+                      <div className="flex flex-1 flex-col items-start">
+                        <p>{mlbbUsername}</p>
+                        <p className="text-sm">
+                          {mlbbUserId} ({mlbbServerId})
+                        </p>
+                        <Badge className="mt-1.5" variant="secondary">
+                          {mlbbRoleEnumToText(role)}
+                        </Badge>
+                      </div>
+                      <div className="h-5 w-5 rounded-full border group-hover:bg-muted"></div>
+                    </div>
+                  );
+                })}
+            </div>
+          </ScrollArea>
+          <SheetFooter className="flex flex-col gap-4 border-t-2 pt-2">
+            <SheetTitle>Player terpilih:</SheetTitle>
+            {/* TODO: use actual data */}
+            <div className="flex items-start gap-4">
+              <div className="flex w-16 flex-col flex-wrap justify-center gap-1.5">
+                <Avatar className="h-16 w-16 rounded-md">
+                  <AvatarImage src="http://localhost:54321/storage/v1/object/public/profile-pictures/1697129641854-d749c822-58a1-4101-a0ba-db8dc6823933.jpeg" />
+                  <AvatarFallback>VF</AvatarFallback>
+                </Avatar>
+                <p className="text-center text-sm text-muted-foreground">
+                  Full
+                </p>
+              </div>
+              <div className="flex flex-col items-start">
+                <p>Nickname</p>
+                <p>12343212 (2211)</p>
+                <Badge className="mt-1.5" variant="secondary">
+                  {mlbbRoleEnumToText(MlbbRole.JUNGLE)}
+                </Badge>
+              </div>
+            </div>
+            <Button>Pilih player</Button>
+          </SheetFooter>
         </SheetContent>
       </Sheet>
     </div>
