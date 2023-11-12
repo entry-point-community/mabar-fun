d96096f3b4950e031a225d9d5ae7135783535c5b
Theodore Rivaldo Devo Mangowal <87703322+theodevoid@users.noreply.github.com>
2023-11-13 01:32:18 +0700

    chore: prep feature flags

--- apps/web/src/env.mjs
+++ apps/web/src/env.mjs
@@ -6,10 +6,14 @@ export const env = createEnv({
     NEXT_PUBLIC_API_URL: z.string().min(1),
     NEXT_PUBLIC_SUPABASE_URL: z.string().min(1),
     NEXT_PUBLIC_SUPABASE_KEY: z.string().min(1),
+    // NEXT_PUBLIC_POSTHOG_KEY: z.string().min(1),
+    // NEXT_PUBLIC_POSTHOG_HOST: z.string().min(1),
   },
   runtimeEnv: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY,
+    // NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+    // NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   },
 });
