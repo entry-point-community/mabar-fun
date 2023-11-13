8fe878c865392febf9b2f0cc53962c53c81bdd08
Prasetya Wijaya <prs.wjy@gmail.com>
2023-11-13 23:34:10 +0800

    refactor: using startOfDay from date-fns

--- packages/dto/src/validators/createDateNowValidator.ts
+++ packages/dto/src/validators/createDateNowValidator.ts
@@ -2,16 +2,14 @@ import {
   ValidatorConstraint,
   ValidatorConstraintInterface,
 } from 'class-validator';
-import { getUnixTime } from 'date-fns';
+import { getUnixTime, startOfDay } from 'date-fns';
 
 // to validate if input time is lower than current time
 export function createDateNowValidator() {
   @ValidatorConstraint({ name: 'dateNowValidator', async: false })
   class DateNowValidator implements ValidatorConstraintInterface {
     validate(value: any) {
-      const currentDate = new Date();
-      const startOfCurrentDate = new Date(currentDate.setHours(0, 0, 0, 0));
-      return getUnixTime(new Date(value)) > getUnixTime(startOfCurrentDate);
+      return getUnixTime(new Date(value)) > getUnixTime(startOfDay(new Date()));
     }
 
     defaultMessage() {
