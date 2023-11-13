4206e8dd8f4c71900e463236f2c06e54c65dad4a
Theodore Rivaldo Devo Mangowal <87703322+theodevoid@users.noreply.github.com>
2023-11-13 22:05:32 +0700

    fix(profile): use upsert instead of update

--- packages/dto/src/profile/edit-profile.dto.ts
+++ packages/dto/src/profile/edit-profile.dto.ts
@@ -1,24 +1,20 @@
 import { MlbbRole } from '@v6/db';
-import { IsEnum, IsOptional, IsString, Length } from 'class-validator';
+import { IsEnum, IsString, Length } from 'class-validator';
 
 export class EditProfileDTO {
   @IsString()
-  @IsOptional()
   @Length(4, 16)
-  readonly mlbbUserId?: string;
+  readonly mlbbUserId: string;
 
   @IsString()
-  @IsOptional()
   @Length(2, 7)
-  readonly mlbbServerId?: string;
+  readonly mlbbServerId: string;
 
   @IsString()
-  @IsOptional()
   @IsEnum(MlbbRole)
-  readonly mlbbRole?: MlbbRole;
+  readonly mlbbRole: MlbbRole;
 
   @IsString()
-  @IsOptional()
   @Length(3, 20)
-  readonly displayName?: string;
+  readonly displayName: string;
 }
